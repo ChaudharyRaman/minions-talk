e6a790eebe95279b5e18b9974702afb3180ea095
Prajjwal kapoor <68912239+prajjwalkapoor@users.noreply.github.com>
2021-04-13 03:20:23 +0530

    app woking condition

---
+++ script.js
@@ -0,0 +1,30 @@
+var userText = document.querySelector("#textarea") ;
+var btn = document.querySelector("#btn-translate") ;
+var output = document.querySelector("#output-div") ;
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function errorHandeler(error) {
+    console.log("error occured" , error)
+    alert("something is not right. try again after some time")
+}
+
+btn.addEventListener("click" , function(){
+
+    function urlConstructor(){
+        return serverURL + "?text=" + userText.value
+    }
+var newURL = urlConstructor(userText)
+console.log(newURL)
+fetch(newURL)
+.then(response => response.json())
+.then(json => {
+    var translatedText = json.contents.translated
+    output.innerHTML = translatedText;})
+.catch(errorHandeler)
+})
+
+
+
+
+
